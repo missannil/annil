1c6f36c03077bdaf6a6c95b6324cb03c34440290
missannil <missannil@163.com>
2023-06-15 03:53:10 +0800

    chore: 修改注释

--- test/types/MainComponent/computed/this.Data.ts
+++ test/types/MainComponent/computed/this.Data.ts
@@ -4,9 +4,6 @@ import { MainComponent } from "../../../../src/api/MainComponent";
 import type { Mock_User } from "../../mockData";
 
 // ----------------computed中的this.data----------------
-/**
- * 与inject重复类型 对象类型
- */
 MainComponent({
   properties: {
     aaa: String,
@@ -19,10 +16,10 @@ MainComponent({
       type: Number,
       optionalTypes: [Object as SpecificType<Mock_User>],
     },
-	optinal:{
-		type:String,
-		value:'123',
-	}
+    optinal: {
+      type: String,
+      value: "123",
+    },
   },
   data: {
     bbb: 123,
@@ -33,7 +30,7 @@ MainComponent({
       ValueChecking<AnyObject | null>()(this.data.obj);
       ValueChecking<Mock_User | number | null>()(this.data.union_obj);
       ValueChecking<Mock_User | number>()(this.data.union_obj1);
-	  ValueChecking<string>()(this.data.optinal);
+      ValueChecking<string>()(this.data.optinal);
       ValueChecking<number>()(this.data.bbb);
       ValueChecking<"123">()(this.data.zzz);
 

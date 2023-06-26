63a0450774c882304a9b7500cf50a3f063220ed0
missannil <missannil@163.com>
2023-06-26 12:15:02 +0800

    perf: data

--- src/api/MainComponents/Data/GetDataDoc.ts
+++ src/api/MainComponents/Data/GetDataDoc.ts
@@ -1,33 +1,28 @@
-import type { IfEquals } from "hry-types/src/Any/IfEquals";
-import type { IfExtends } from "hry-types/src/Any/IfExtends";
-import type { Filter } from "hry-types/src/Object/Filter";
-import type { ReturnTypeInObject } from "hry-types/src/Object/ReturnTypeInObject";
-import type { Select } from "hry-types/src/Object/Select";
+import type { A, O } from "hry-types";
 import type { DataConstraint } from "./DataConstraint";
+
 /**
- *  @description DataKeyTypes
+ *  @description ReturnTypes
  */
-export type DataKeyTypes = "去掉函数字段" | "返回函数字段" | "函数值类型变为函数返回类型";
+type ReturnTypes = "去掉函数字段" | "返回函数字段" | "函数值类型变为函数返回类型";
 
 /**
  * @description 获取data字段文档类型
  * @param T DataConstraint
- * @param TType DataKeyTypes
+ * @param TType ReturnTypes
  * @returns AnyObject
  */ export type GetDataDoc<
   TData extends DataConstraint,
-  TType extends DataKeyTypes = "函数值类型变为函数返回类型",
-> = IfExtends<
-  TType,
+  Type extends ReturnTypes = "函数值类型变为函数返回类型",
+> = A.IfExtends<
+  Type,
   "函数值类型变为函数返回类型",
-  ReturnTypeInObject<TData>,
-  IfExtends<
-    TType,
+  O.ReturnTypeInObject<TData>,
+  A.IfExtends<
+    Type,
     "去掉函数字段",
-    IfEquals<Filter<TData, AnyFunction>, {}, unknown>,
+    A.IfEquals<O.Filter<TData, AnyFunction>, {}, unknown>,
     // "返回函数字段"
-    Select<TData, AnyFunction>
+    O.Select<TData, AnyFunction>
   >
 >;
-
-// Path: src\api\MainComponents\Data\GetDataDoc.ts
--- src/api/MainComponents/Data/test/GetDataDoc.test.ts
+++ src/api/MainComponents/Data/test/GetDataDoc.test.ts
@@ -1,16 +1,25 @@
 import { type Test, TypeChecking } from "hry-types";
 
+import type { DataConstraint } from "../DataConstraint";
 import type { GetDataDoc } from "../GetDataDoc";
 // ----------------GetDataDoc泛型测试----------------
 
-type testData = { a: () => number; b: number; c: string };
+const dataFields = {
+  a: () => 1,
+  b: 2,
+  c: "3",
+} satisfies DataConstraint;
 
-TypeChecking<GetDataDoc<testData, "函数值类型变为函数返回类型">, { a: number; b: number; c: string }, Test.Pass>;
+TypeChecking<
+  GetDataDoc<typeof dataFields, "函数值类型变为函数返回类型">,
+  { a: number; b: number; c: string },
+  Test.Pass
+>;
 
-TypeChecking<GetDataDoc<testData, "去掉函数字段">, { b: number; c: string }, Test.Pass>;
+TypeChecking<GetDataDoc<typeof dataFields, "去掉函数字段">, { b: number; c: string }, Test.Pass>;
 
-TypeChecking<GetDataDoc<testData, "返回函数字段">, { a: () => number }, Test.Pass>;
+TypeChecking<GetDataDoc<typeof dataFields, "返回函数字段">, { a: () => number }, Test.Pass>;
 
-type dataDefault = {};
+const empty_object = {};
 
-TypeChecking<GetDataDoc<dataDefault>, {}, Test.Pass>;
+TypeChecking<GetDataDoc<typeof empty_object>, {}, Test.Pass>;

ebac03314b26d605679b95c2c0e23ef293e1568a
missanni <missannil@163.com>
2024-09-07 14:05:52 +0800

    fix: 优化_SubComponentType类型,避免接口或类的类型由于无签名照成的报错
    优化utils函数

--- src/utils/assertNonNullable.ts
+++ src/utils/assertNonNullable.ts
@@ -1,5 +1,10 @@
 import type { IfContains } from "hry-types/src/Any/IfContains";
 
+/**
+ * 使用此函数代替!断言
+ * @param value 类型中必须包含 null 或 undefined
+ * @returns 去除 null 和 undefined 后的类型
+ */
 export function assertNonNullable<T>(
   value: IfContains<T, null | undefined, T, "参数类型必须包含 null 或 undefined">,
 ): NonNullable<T> {

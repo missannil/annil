8d1575d6d84a1073043ed3137a4c70eb6ec427fc
missanni <missannil@163.com>
2025-01-13 10:41:57 +0800

    ci: 使用action artifacts@v4,去除重复运行生成覆盖率报告

--- jest.config.ci.ts
+++ jest.config.ci.ts
@@ -2,5 +2,7 @@ import { config } from "./jest.config";
 
 exports.default = {
   ...config, // 导入原始的 Jest 配置
+  collectCoverage: true, // 收集测试覆盖率
+  coverageDirectory: "coverage", // 指定覆盖率报告的输出目录
   coverageReporters: ["lcov"], // 在 CI 中使用 'lcov' 格式
 };

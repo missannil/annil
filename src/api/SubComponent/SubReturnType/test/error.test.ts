433779f12e7a3549a77d87c2663d37405ea2692f
missanni <missannil@163.com>
2025-01-14 14:22:05 +0800

    chore: release alpha.4
    release-as: 1.11.0-alpha.4

--- src/api/SubComponent/SubReturnType/test/error.test.ts
+++ src/api/SubComponent/SubReturnType/test/error.test.ts
@@ -12,7 +12,7 @@ type CompDoc = ComponentType<{
 
 const SubDoc = SubComponent<{}, CompDoc>()({});
 void SubDoc;
-// 返回穿透的自定义事件,并去除了前缀
+// 返回穿透的自定义事件, 并去除了前缀
 type SubDocExpect = "缺少必传的字段aaa_str、aaa_bool";
 
 Checking<typeof SubDoc, SubDocExpect, Test.Pass>();

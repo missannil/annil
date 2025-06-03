c91946878c10ecf0f434ec8b9a7d2e983f20b5db
hry-missannil <missannil@163.com>
2025-06-03 16:40:12 +0800

    docs: 使用vitePress建立文档

--- src/api/RootComponent/Store/GeTStoreDoc.ts
+++ src/api/RootComponent/Store/GeTStoreDoc.ts
@@ -1,25 +1,28 @@
 import type { ParamsEqual } from "../../../types/TwoParamsEqual";
 import type { Getter } from "./StoreConstraint";
-
-type UndefinedToNull<T> = T extends undefined ? null : T;
-
 // 单独Getter类型返回值会把undefined转换为null
 // 但是WithDefault类型返回值为getter的返回值去除undefined和default的类型联合。
 export type GetStoreDoc<TStore extends object> = {
-  [k in keyof TStore]: TStore[k] extends Getter ? UndefinedToNull<ReturnType<TStore[k]>>
+  [k in keyof TStore]: TStore[k] extends Getter ? ReturnType<TStore[k]>
     : TStore[k] extends {
-      getter: () => infer R;
+      getter: (...args: unknown[]) => infer R;
       default: infer D;
-    } ? Exclude<R, undefined> | D
+    } ? Exclude<R, undefined> | (D extends null ? null : never)
     : never;
 };
+
+// test
 type User = { name: string; age: number };
 type test = GetStoreDoc<{
-  userName: () => string;
-  userAge: () => number;
+  userName: (data: { aaa: "string" }) => string;
+  userAge: (data: { aaa: "string" }) => number;
   userInfo: {
-    getter: () => User | undefined;
+    getter: (data: { aaa: "string" }) => User | undefined;
     default: null;
+  };
+  list: {
+    getter: (data: { aaa: "string" }) => User[] | undefined;
+    default: [];
   };
 }>;
 
@@ -30,5 +33,6 @@ type test1 = ParamsEqual<
     userName: string;
     userAge: number;
     userInfo: User | null;
+    list: User[];
   }
 >;
